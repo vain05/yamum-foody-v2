3fa05a53e46cabd805fcb4ef5a9e377d789aad39
vain05 <truongduy20226@gmail.com>
2022-08-23 14:38:01 +0700

    initial commit

--- App.js
+++ App.js
@@ -1,20 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+
+import { StyleSheet, Text, View } from "react-native";
+
+import { createStackNavigator } from "@react-navigation/stack";
+
+import { NavigationContainer } from "@react-navigation/native";
+
+import { MainLayout } from "./screens";
+
+import CustomDrawer from "./navigation/CustomDrawer";
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName={"Home"}
+        screenOptions={{
+          headShown: false,
+        }}
+      >
+        <Stack.Screen
+          name="Home"
+          component={CustomDrawer}
+        />
+
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});

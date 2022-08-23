2a9bbcfb0cccdda3b1890074cd405bd43cf85d78
Steki3 <cuong24112002@gmail.com>
2022-08-23 20:27:52 +0700

    Top of main layout

--- screens/MainLayout.js
+++ screens/MainLayout.js
@@ -1,21 +1,109 @@
 import React from 'react';
 import {
     View,
-    Text
-} from 'react-native';
+    Text,
+    TouchableOpacity,
+    TouchableWithoutFeedback,
+    Image,
+    FlatList
 
-const MainLayout = () => {
+} from 'react-native';
+import Animated, {
+    useSharedValue,
+    useAnimatedStyle,
+    withTiming,
+
+
+} from "react-native-reanimated";
+
+import {  connect  } from "react-redux";
+import {  setSelectedTab  } from "../stores/tab/tabActions";
+import {
+    Home,
+    Search,
+    CartTab,
+    Favourite,
+    Notification
+} from "../screens";
+import {Header} from "../components";
+import {
+    COLORS,
+    FONTS,
+    SIZES,
+    icons,
+    constants,
+    dummyData
+} from "../constants"
+
+const MainLayout = ({drawerAnimationStyle, navigation,selectedTab,setSelectedTab}) => {
+    
+    React.useEffect(() => {
+        setSelectedTab(constants.screens.home)
+    }, [])
+    
     return (
-        <View
+        <Animated.View
             style={{
                 flex: 1,
                 alignItems: 'center',
-                justifyContent: 'center'
+                justifyContent: 'center',
+                backgroundColor: 'white',
+                ...drawerAnimationStyle
             }}
         >
-            <Text>MainLayout</Text>
+            {/* Header */}
+            <Headers
+                containerStyle = {{
+                    height: 50,
+                    paddingHorizontal: SIZES.padding,
+                    marginTop: 40,
+                    alignItems: 'center'
+                }}
+                title = {selectedTab.toUpperCase()}
+                left = {
+                    <TouchableOpacity
+                        style = {{
+                            width: 40,
+                            height: 40,
+                            alignItems: 'center',
+                            justifyContent: 'center',
+                            borderWidth: 1,
+                            borderColor: COLORS.gray2,
+                            borderRadius
+                        }}
+                        onPress = {() => navigation.openDrawer()}
+                    >
+                        <Image
+                            source = {icons.menu}
+                        />
+                        
+                    </TouchableOpacity>
+                }
+            />
+            {/* Content */}
+            <View
+                style = {{
+                    flex: 1
+                }}
+            >
+                <Text>MainLayout</Text>
+            </View>
+            {/* Footer */}
         </View>
     )
 }
 
-export default MainLayout;
+function mapStateToProps(state) {
+    return {
+        selectedTab: state.tabReducer.selectedTab
+    }
+}
+
+function mapDispatchToProps(state) {
+    return {
+        setSelectedTab: (selectedTab) => {return dispatch
+        (setSelectedTab(selectedTab))}
+    }
+}
+
+export default connect{mapStateToProps, mapDispatchToProps} (MainLayout)
